7a2782e745e76c118ad0b213b51987ce7df3ec90
arseniyanson <6653312@gmail.com>
2021-01-06 09:42:55 +0000

    через else if поправили работу женщины до пенсионногог

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ var ageInDays = age * 365;
 var ageWithFive = (age * 1) + 5;
 const menPensionAge = 62;
 const womenPensionAge = 57;
-if (wmen == "женский" && (age * 1) < womenPensionAge)
+if (wmen === "женский" & (age * 1) < womenPensionAge)
   var pensionAge = "нет";
-if (wmen == "мужской" && (age * 1) < menPensionAge)
+else if (wmen === "женский" & (age * 1) >= menPensionAge)
+  var pensionAge = "да";
+else if (wmen === "мужской" & (age * 1) < menPensionAge)
   var pensionAge = "нет";
-else
-  pensionAge = "да";
+else if (wmen === "мужской" & (age * 1) >= menPensionAge)
+  var pensionAge = "да";
 
 alert(`    Ваше фио: ${lastName} ${firstName} 
     Ваш возраст в годах: ${age}
